57a186d5fff97c29f653b4ea205e54b0d2c1bdd9
희호 <kim21670@naver.com>
2026-05-16 15:29:29 +0900

    fix: useEffect 중복 실행 방지 처리

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import TodoApp from './components/TodoApp'
 import useTodosState from './hooks/useTodosState';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import './styles/common.css';
 function App() {
  const todosState = useTodosState();
  
+ const initialized = useRef(false);
+
  // useEffect를 활용하여 앱이 실행될 때 기본 할 일을 추가
- useEffect(()=>{
+ useEffect(() => {
+  if (initialized.current) return;
+
+  initialized.current = true;
   todosState.addTodo('react 공부하기');
   todosState.addTodo('정처기 공부하기');
   todosState.addTodo('기말고사 준비하기');
